b242ef840db3449477f3eb62a818551e7fa5880b
Dasom <dev.dasom1012@gmail.com>
2024-08-31 15:16:16 +0900

    WebSocket Messages

--- src/server.js
+++ src/server.js
@@ -15,9 +15,16 @@ const handleListen = () => console.log(`Listening on http://localhost:3000`);
 const server = http.createServer(app);
 const wss = new WebSocketServer({ server });
 
-const handleConnection = (socket) => {
-	console.log(socket);
-};
-wss.on('connection', handleConnection);
+wss.on('connection', (socket) => {
+	console.log('Connected to Browser');
+	socket.on('close', () => {
+		console.log('Disconnected from the Browser ❌');
+	});
+	socket.on('message', (message, isBinary) => {
+		message = isBinary ? message : message.toString();
+		console.log(message);
+	});
+	socket.send('hello!!');
+});
 
 server.listen(3000, handleListen);
