dd535128483ec378cb59ae0dec07b434636cad81
Dasom <dev.dasom1012@gmail.com>
2024-08-31 14:48:58 +0900

    WebSocket Connection

--- src/server.js
+++ src/server.js
@@ -1,3 +1,5 @@
+import http from 'http';
+import { WebSocketServer } from 'ws';
 import express from 'express';
 
 const app = express();
@@ -5,9 +7,17 @@ const app = express();
 app.set('view engine', 'pug');
 app.set('views', __dirname + '/views');
 app.use('/public', express.static(__dirname + '/public'));
+app.get('/', (_, res) => res.render('home'));
+app.get('/*', (_, res) => res.redirect('/'));
 
-app.get('/', (req, res) => res.render('home'));
+const handleListen = () => console.log(`Listening on http://localhost:3000`);
 
-app.listen(3000, () => {
-	console.log('Server started on port 3000');
-});
+const server = http.createServer(app);
+const wss = new WebSocketServer({ server });
+
+const handleConnection = (socket) => {
+	console.log(socket);
+};
+wss.on('connection', handleConnection);
+
+server.listen(3000, handleListen);
