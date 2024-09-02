04ce46fb7c462526b50eaa8b3f74dca22a04ae17
Dasom <dev.dasom1012@gmail.com>
2024-09-02 13:32:54 +0900

    Room Messages

--- src/server.js
+++ src/server.js
@@ -16,10 +16,31 @@ const httpServer = http.createServer(app);
 const wsServer = SocketIO(httpServer);
 
 wsServer.on('connection', (socket) => {
-	socket.on('enter_room', (msg, done) => {
-		console.log('message', msg);
-		setTimeout(() => done(), 2000);
+	socket['nickname'] = 'Anon';
+
+	socket.onAny((event) => {
+		console.log(`Socket Event:${event}`);
 	});
+
+	socket.on('enter_room', (roomName, done) => {
+		socket.join(roomName);
+		done();
+
+		socket.to(roomName).emit('welcome', socket.nickname);
+	});
+
+	socket.on('disconnecting', () => {
+		socket.rooms.forEach((room) =>
+			socket.to(room).emit('bye', socket.nickname)
+		);
+	});
+
+	socket.on('new_message', (msg, room, done) => {
+		socket.to(room).emit('new_message', `${socket.nickname}: ${msg}`);
+		done();
+	});
+
+	socket.on('nickname', (nickname) => (socket['nickname'] = nickname));
 });
 
 // const wss = new WebSocketServer({ server });
