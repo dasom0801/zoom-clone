af9518984de4c1ebbe951eff2afda38fdaa65b50
Dasom <dev.dasom1012@gmail.com>
2024-09-02 13:59:42 +0900

    Room Count

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -60,3 +60,17 @@ socket.on('bye', (user) => {
 });
 
 socket.on('new_message', addMessage);
+
+socket.on('room_change', (rooms) => {
+	const roomList = document.querySelector('#welcome ul');
+	roomList.innerHTML = '';
+	if (rooms.length === 0) {
+		return;
+	}
+
+	rooms.forEach((room) => {
+		const li = document.createElement('li');
+		li.innerText = room;
+		roomList.append(li);
+	});
+});
