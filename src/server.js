f5791ec843caf2f23148f914a6ed06a72830c288
Dasom <dev.dasom1012@gmail.com>
2024-09-02 14:05:36 +0900

    User Count

--- src/server.js
+++ src/server.js
@@ -31,6 +31,10 @@ function publicRooms() {
 	return publicRooms;
 }
 
+function countRoom(roomName) {
+	return wsServer.sockets.adapter.rooms.get(roomName)?.size;
+}
+
 wsServer.on('connection', (socket) => {
 	socket['nickname'] = 'Anon';
 
@@ -42,13 +46,13 @@ wsServer.on('connection', (socket) => {
 		socket.join(roomName);
 		done();
 
-		socket.to(roomName).emit('welcome', socket.nickname);
+		socket.to(roomName).emit('welcome', socket.nickname, countRoom(roomName));
 		wsServer.sockets.emit('room_change', publicRooms());
 	});
 
 	socket.on('disconnecting', () => {
 		socket.rooms.forEach((room) =>
-			socket.to(room).emit('bye', socket.nickname)
+			socket.to(room).emit('bye', socket.nickname, countRoom(room) - 1)
 		);
 	});
 
